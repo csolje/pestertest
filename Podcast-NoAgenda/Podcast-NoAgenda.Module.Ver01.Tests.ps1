c16948b334fc6d1b1e881eae85a8e509a1e168f9
Christian Sølje <csolje@gmail.com>
2017-10-12 00:03:16 +0200

    Added all the function tests

--- Podcast-NoAgenda/Podcast-NoAgenda.Module.Ver01.Tests.ps1
+++ Podcast-NoAgenda/Podcast-NoAgenda.Module.Ver01.Tests.ps1
@@ -55,4 +55,16 @@ Describe "$module Module Tests" {
 		It "Write-PodcastHtml should exist" {
 			"$here\function-Write-PodcastHtml.ps1" | Should Exist
 		}
+
+		It "Write-PodcastXML.ps1 should exist" {
+			"$here\function-Write-PodcastXML.ps1" | Should Exist
+		}
+
+		It "Format-PodcastHtml.ps1 should exist" {
+			"$here\function-Format-PodcastHtml.ps1" | Should Exist
+		}
+
+		It "Format-PodcastXML.ps1 should exist" {
+			"$here\function-Format-PodcastXML.ps1" | Should Exist
+		}
 	}
